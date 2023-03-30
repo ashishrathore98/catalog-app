1ee75de8bcb76b887d1d78c7a1ae725319005106
Ashish Rathore <89871449+ashishrathore98@users.noreply.github.com>
2023-03-31 01:12:10 +0530

    Add files via upload

---
+++ recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:food_recipe/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "24", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "X-RapidAPI-Key": "0459cecb22mshd8eaeabfd6013f8p13c5eejsnb17224abd394",
+	"X-RapidAPI-Host": "yummly2.p.rapidapi.com",
+	"useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
